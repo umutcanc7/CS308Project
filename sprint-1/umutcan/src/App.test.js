aba49f2bf9f6e67462dfae4876f91d1336fec3b0
Umut Can Çubukçu <umutcancubukcu@Umut-MacBook-Pro.local>
2025-03-07 16:39:11 +0300

    Removed unnecessary files from sprint-1

--- sprint-1/umutcan/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
-
-test("renders login page", () => {
-  render(<App />);
-  const heading = screen.getByText(/Hoş Geldiniz/i);
-  expect(heading).toBeInTheDocument();
-});
