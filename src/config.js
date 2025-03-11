7f212b00962f4dd49b1099dc45ff6054884718eb
ArdaSug <sugurarda04@gmail.com>
2025-03-11 18:12:00 +0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -4,8 +4,7 @@ require("dotenv").config();
 const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
+            
         });
         console.log("✅ MongoDB Connected...");
     } catch (err) {
