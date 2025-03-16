d8150babd14e93cc6a8227e0a9c564f3cdc15d07
BatuhanTheFutureSoftwareDev <148706187+BatuhanTheFutureSoftwareDev@users.noreply.github.com>
2025-03-16 13:24:37 +0300

    Update Menu.js

--- sprint1baglanmis/src/Menu.js
+++ sprint1baglanmis/src/Menu.js
@@ -1,28 +1,50 @@
-// Menu.js
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 import "./Menu.css";
 
 function Menu() {
+  const [isOpen, setIsOpen] = useState(false);
+  const menuRef = useRef(null);
+
+  const toggleMenu = () => setIsOpen(!isOpen);
+
+  // Close menu if clicking outside of it
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (isOpen && menuRef.current && !menuRef.current.contains(event.target)) {
+        setIsOpen(false);
+      }
+    }
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => document.removeEventListener("mousedown", handleClickOutside);
+  }, [isOpen]);
+
   return (
-    <div className="menu">
-      <div className="hamburger-icon">
+    <>
+      {/* Hamburger icon: click to toggle the menu */}
+      <div className="hamburger-icon" onClick={toggleMenu}>
         <span></span>
         <span></span>
         <span></span>
       </div>
-      <div className="menu-items">
-        <Link to="/" className="menu-item">
+      
+      {/* Sliding menu */}
+      <div ref={menuRef} className={`sliding-menu ${isOpen ? "open" : ""}`}>
+        {/* Close button inside menu */}
+        <button className="close-btn" onClick={toggleMenu}>
+          &times;
+        </button>
+        <Link to="/home" className="menu-item" onClick={toggleMenu}>
           Home
         </Link>
-        <Link to="/shop" className="menu-item">
+        <Link to="/shop" className="menu-item" onClick={toggleMenu}>
           Shop
         </Link>
-        <Link to="/magazines" className="menu-item">
+        <Link to="/magazines" className="menu-item" onClick={toggleMenu}>
           Magazines
         </Link>
       </div>
-    </div>
+    </>
   );
 }
 
