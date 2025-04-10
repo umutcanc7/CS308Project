29229d81da642ca8a6eebc048bbaaa73573569a9
BatuhanTheFutureSoftwareDev <148706187+BatuhanTheFutureSoftwareDev@users.noreply.github.com>
2025-04-10 13:54:08 +0300

    Delete sprint1baglanmis directory

--- sprint1baglanmis/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
-
-test("renders login page", () => {
-  render(<App />);
-  const heading = screen.getByText(/Welcome!/i);
-  expect(heading).toBeInTheDocument();
-});
